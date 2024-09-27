1983d3bc2c497f2658470025da5430eb3487cd9a
Usha Ramchandra Kudale <129097741+Ushakudale1511@users.noreply.github.com>
2024-09-27 21:36:18 +0530

    Create PositiveNegative.java

---
+++ PositiveNegative.java
@@ -0,0 +1,14 @@
+class A {
+  public static void main (String []args){
+    int num = 78;
+    if(num>0){
+      System.out.println("Number is Positive");
+        }
+    elseif(num<0){
+      System.out.println("Number is Negative");
+    }
+    else{
+      System.out.println("Number is Zero");
+  }
+}
+}
