d8e4179366718d4cdf21698c9ac4ca02b6a4d478
Usha Ramchandra Kudale <129097741+Ushakudale1511@users.noreply.github.com>
2024-09-27 21:38:58 +0530

    Create EvenOdd.java

---
+++ EvenOdd.java
@@ -0,0 +1,11 @@
+class even{
+  public static void main (String args[]){
+    int num=8;
+    if(num%2==0){
+      System.out.println("Number is Even");
+    }
+    else{
+      System.out.println("Number is odd");
+    }
+  }
+}
