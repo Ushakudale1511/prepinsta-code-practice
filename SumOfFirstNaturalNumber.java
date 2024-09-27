5bd76e5409aac01c3464b56daf4f43e95f26c07c
Usha Ramchandra Kudale <129097741+Ushakudale1511@users.noreply.github.com>
2024-09-27 21:51:03 +0530

    Update SumOfFirstNaturalNumber.java

--- SumOfFirstNaturalNumber.java
+++ SumOfFirstNaturalNumber.java
@@ -4,3 +4,17 @@ class natural{
     System.out.println(num*(num+1)/2);
   }
 }
+// using user input :
+// import java.util.Scanner;
+// class natural{
+//   public static void main (String []args){
+//     int num;
+    
+//     Scanner sc= new Scanner(System.in);
+//     System.out.println("Enter a number:");
+//     num=sc.nextInt();
+    
+//     System.out.println( " sum of given natural number is :" +num*(num+1)/2);
+    
+//   }
+// }
